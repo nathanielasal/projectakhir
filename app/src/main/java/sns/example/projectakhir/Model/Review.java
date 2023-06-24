8c15c5ac500869d7d46ba60ce2da748fe05634d2
nathanielasal <120065276+nathanielasal@users.noreply.github.com>
2023-06-24 20:27:16 +0700

    update

--- app/src/main/java/sns/example/projectakhir/Model/Review.java
+++ app/src/main/java/sns/example/projectakhir/Model/Review.java
@@ -49,59 +49,6 @@ public class Review  {
     public void setRateCount(String rateCount) {
         this.rateCount = rateCount;
     }
-//
-//    protected Review(Parcel in) {
-//        // Baca data dari Parcel ke dalam field objek
-//        this.reviewInput = in.readString();
-//        this.rateCount = in.readString();
-//    }
-//
-//    @Override
-//    public void writeToParcel(Parcel dest, int flags) {
-//        // Tulis data dari field objek ke dalam Parcel
-//        dest.writeString(rateCount);
-//        dest.writeString(reviewInput);
-//    }
-//
-//    @Override
-//    public int describeContents() {
-//        return 0;
-//    }
-//
-//    // Implementasikan creator untuk Parcelable
-//    public static final Creator<Review> CREATOR = new Creator<Review>() {
-//        @Override
-//        public Review createFromParcel(Parcel in) {
-//            return new Review(in);
-//        }
-//
-//        @Override
-//        public Review[] newArray(int size) {
-//            return new Review[size];
-//        }
-
-//    public void readFromParcel(Parcel source) {
-//        this.key = source.readString();
-//        this.reviewInput = source.readString();
-//        this.rateCount = source.readString();
-//    }
-//    protected Review(Parcel in) {
-//        this.key = in.readString();
-//        this.reviewInput = in.readString();
-//        this.rateCount = in.readString();
-//    }
-//
-//    public static final Parcelable.Creator<Review> CREATOR = new Parcelable.Creator<Review>() {
-//        @Override
-//        public Review createFromParcel(Parcel source) {
-//            return new Review(source);
-//        }
-//
-//        @Override
-//        public Review[] newArray(int size) {
-//            return new Review[size];
-//        }
-//    };
     };
 
 
